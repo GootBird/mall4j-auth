f235232f41ed9d8e8b1f180cb50dc79034aeaeef
GootBird <xixichen698@outlook.com>
2022-09-20 03:36:57 +0800

    孙笑川打老奶奶

--- src/main/java/com/xixi/mall/auth/config/SwaggerConfiguration.java
+++ src/main/java/com/xixi/mall/auth/config/SwaggerConfiguration.java
@@ -25,7 +25,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.xixi.mall4j.auth.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.xixi.mall.auth.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
